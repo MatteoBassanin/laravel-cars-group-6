005e6b043175c462370341547604f2067f904cd5
Alex Nunzi <alex.nunzi@hotmail.it>
2023-05-26 12:40:17 +0200

    Sistemato task 2

---
+++ resources/views/cars/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container p-5">
+        <div class="card mb-5">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{ $car->brand }}</li>
+                <li class="list-group-item">{{ $car->model }}</li>
+                <li class="list-group-item">{{ $car->price }}</li>
+                <li class="list-group-item">{{ $car->cc }}</li>
+                <li class="list-group-item">{{ $car->year_release }}</li>
+            </ul>
+        </div>
+        <a class="btn btn-primary" href="{{ route('cars.index') }}">Go back</a>
+    </div>
+@endsection
