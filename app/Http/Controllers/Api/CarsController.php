e41ffb70afc967bce4acab3b327036e9e347c16e
Alex Nunzi <alex.nunzi@hotmail.it>
2023-06-05 16:06:58 +0200

    Task 4 rotte API

---
+++ app/Http/Controllers/Api/CarsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Car;
+use Illuminate\Http\Request;
+
+class CarsController extends Controller
+{
+    public function index()
+    {
+        $cars = Car::all();
+
+        if ($cars) {
+            return response()->json([
+                'success' => true,
+                'cars' => $cars
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'errors' => 'Nessuna macchina trovata'
+            ]);
+        }
+    }
+
+    public function show($id)
+    {
+        $car = Car::where('id', $id)->first();
+
+        if ($car) {
+            return response()->json([
+                'success' => true,
+                'car' => $car
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'errors' => 'Nessuna macchina trovata'
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CarsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/cars', [CarsController::class, 'index']);
+
+Route::get('/cars/{id}', [CarsController::class, 'show']);
